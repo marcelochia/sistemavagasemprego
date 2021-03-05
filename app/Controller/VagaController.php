ca878adab7bd1d763d3a40f05d3f4d2a1ab4b180
Marcelo Henrique Chiaretto <mhchiaretto@gmail.com>
2021-03-04 23:36:40 -0300

    Remodelado VagaController para receber os parâmetros das rotas

--- app/Controller/VagaController.php
+++ app/Controller/VagaController.php
@@ -3,36 +3,52 @@
 namespace App\Controller;
 
 use App\Model\Vaga;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 class VagaController
 {
-    public static function index()
+    public static function index(Request $request, Response $response)
     {
         $vagas = new Vaga();
-        return $vagas->listarTodos();
+        $vagas = json_encode($vagas->listarTodos(), JSON_PRETTY_PRINT);
+        $response->getBody()->write($vagas);
+        return $response;
     }
 
-    public static function show($id)
+    public static function show(Request $request, Response $response, array $args)
     {
         $vaga = new Vaga();
-        return $vaga->listar($id);
+        $id = $args['id'];
+        $vaga = json_encode($vaga->listar($id), JSON_PRETTY_PRINT);
+        $response->getBody()->write($vaga);
+        return $response;
     }
 
-    public static function insert($valores)
+    public static function create(Request $request, Response $response)
     {
         $vaga = new Vaga();
-        $vaga->inserir($valores);   
+        $data = $request->getParsedBody();
+        $vaga->inserir($data);
+        return $response;
     }
 
-    public static function update($id, $values)
+    public static function update(Request $request, Response $response, array $args)
     {
         $vaga = new Vaga();
+        $id = $args['id'];
+        $values = $request->getParsedBody();
+        // print_r($values);die;
         $vaga->atualizar($id, $values);
+        return $response;
     }
 
-    public static function destroy($id)
+    public static function destroy(Request $request, Response $response, array $args)
     {
+        print_r($request);die;
         $vaga = new Vaga();
+        $id = $args['id'];
         $vaga->apagar($id);
+        return $response;
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
 <?php
-require __DIR__.'/../vendor/autoload.php';
 
+use App\Config;
+use App\Controller\VagaController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+date_default_timezone_set((new Config)->timezone());
+
+$app = AppFactory::create();
+
+$app->addErrorMiddleware(true, false, false);
+
+$app->get('/vagas', VagaController::class . ':index');
+
+$app->post('/vagas/{id}/atualizar', VagaController::class . ':update');
+
+$app->get('/vagas/{id}', VagaController::class . ':show');
+
+$app->post('/vagas/novo', VagaController::class . ':create');
+
+$app->delete('/vagas/{id}/apagar', VagaController::class . ':destroy');
+
+$app->run();
