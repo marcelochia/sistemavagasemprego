a735f99c69375526c6815194ee4728db373c653f
Marcelo Henrique Chiaretto <mhchiaretto@gmail.com>
2021-03-06 11:34:02 -0300

    Retirado namespaces que não estavam sendo utilizados

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 
 use App\Config;
 use App\Controller\VagaController;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
