5d450d42132c3a2930e9c0930bcf49204019cc22
Marcelo Henrique Chiaretto <mhchiaretto@gmail.com>
2021-03-21 15:19:51 -0300

    Funções transferidas para Model

--- app/Model/Vaga.php
+++ app/Model/Vaga.php
@@ -2,13 +2,9 @@
 
 namespace App\Model;
 
-use App\DB\Database;
-use App\DB\QueryBuilder;
-use PDO;
-
-class Vaga
+class Vaga extends Model
 {
-    protected $table = 'vagas';
+    protected $table = 'VAGAS';
 
     protected $columns = [
         'id',
@@ -21,39 +17,4 @@ class Vaga
         'data_criacao',
         'data_alteracao'
     ];
-
-    public function listarTodos()
-    {
-        $query = (new QueryBuilder())->select('*')->from($this->table);
-        $result = (new Database())->execute($query)->fetchAll(PDO::FETCH_ASSOC);
-        return $result;
-    }
-
-    public function exibir(int $id)
-    {
-        $query = (new QueryBuilder())->select('*')->from($this->table)->where("{$this->columns[0]} = {$id}");
-        $result = (new Database())->execute($query)->fetchAll(PDO::FETCH_ASSOC);
-        return $result;
-    }
-
-    public function inserir($values = array())
-    {
-        $query = (new QueryBuilder())->insert($this->table, $values);
-        (new Database())->execute($query, array_values($values));
-    }
-
-    public function atualizar($id, $values = array())
-    {
-        $date = date('Y-m-d H:i:s');
-        $values['data_alteracao'] = $date;
-        
-        $query = (new QueryBuilder())->update($this->table)->set($values)->where("{$this->columns[0]} = {$id}");
-        (new Database())->execute($query, array_values($values));
-    }
-
-    public function apagar($id)
-    {
-        $query = (new QueryBuilder())->delete()->from($this->table)->where("{$this->columns[0]} = {$id}");
-        (new Database())->execute($query);
-    }
 }
